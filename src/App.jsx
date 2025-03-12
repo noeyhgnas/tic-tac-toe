5486b41dd1d95d042c6af80edf00b8d6736bf097
noeyhgnas <shawn2018@naver.com>
2025-03-12 11:21:28 +0900

    create Player Component (Player.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
+import Player from "./components/Player"
+
 function App() {
-  return <main>
-    <div id="game-container">
-      <ol id="players">
-        <li>
-          <span className="player-name">Player 1</span>
-          <span className="player-symbol">X</span>
-        </li>
-        <li> 
-          <span className="player-name">Player 2</span>
-          <span className="player-symbol">O</span>
-        </li>
-      </ol>
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
 
-      GAME BOARD
-    </div>
-
-    LOG
-  </main>
+      LOG
+    </main>
+  )
 }
 
 export default App
